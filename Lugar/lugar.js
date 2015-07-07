3265d5a3093c7f2d4dadab828dda59e9d2ef799a
unknown <dixi_1903@hotmail.com>
2015-07-07 11:21:09 -0500

    Agrege listas de testigos y detenidos

--- Lugar/lugar.js
+++ Lugar/lugar.js
@@ -1,59 +1,186 @@
-/*global angular, google*/ 
+/*global angular, google*/
 
 
-(function () {
-    
+var app = angular.module('App', []);
+
+
+
+app.factory('VehiculeService', function () {
     "use strict";
-    var app = angular.module('App', []);
+    var vehiculos, selectedIndex, selected, returnService;
 
-    app.factory('VehiculeService', function () {
-        var vehiculos, selectedIndex, selected, returnService;
-        
-        vehiculos = [
-        ];
-        
-        selected = 0;
-    
-        returnService = {};
-        
-        returnService.vehiculos = vehiculos;
-        returnService.selected = selected;
-        returnService.add = function () {
-            vehiculos[vehiculos.length] = {};
-        };
-        
-        
-        returnService.remove = function (index) {
-            vehiculos.splice(index, 1);
-        };
-        
-        return returnService;
-        
-    });
+    vehiculos = [
+    ];
 
-    app.controller("VehiculeControl", function ($scope, VehiculeService) {
-        $scope.Vehiculos = VehiculeService.vehiculos;
-        
-        $scope.selected = VehiculeService.selected;
-    
-        $scope.select = function (index) {
-            $scope.selected = index;
-        };
-        
-        $scope.add = function () {
-            $scope.selected = VehiculeService.vehiculos.length;
-            VehiculeService.add();
-        };
-        
-        $scope.remove = function (index) {
-            VehiculeService.remove(index);
-        };
-        
-    });
+    selected = 0;
 
-    app.controller("modificarVehiculo", function ($scope, $routeParams, VehiculeService) {
-        $scope.id = $routeParams.id;
-        $scope.Vehiculo = VehiculeService.vehiculos[$scope.id];
-    });
-    
-}());
+    returnService = {};
+
+    returnService.vehiculos = vehiculos;
+    returnService.selected = selected;
+    returnService.add = function () {
+        vehiculos[vehiculos.length] = {};
+    };
+    returnService.remove = function (index) {
+        vehiculos.splice(index, 1);
+    };
+
+    return returnService;
+
+});
+
+app.controller("VehiculeControl", function ($scope, VehiculeService) {
+    "use strict";
+    $scope.Vehiculos = VehiculeService.vehiculos;
+
+    $scope.selected = VehiculeService.selected;
+
+    $scope.select = function (index) {
+        $scope.selected = index;
+    };
+
+    $scope.add = function () {
+        $scope.selected = VehiculeService.vehiculos.length;
+        VehiculeService.add();
+    };
+
+    $scope.remove = function (index) {
+        VehiculeService.remove(index);
+    };
+
+});
+
+app.factory('VictimasService', function () {
+    "use strict";
+    var victimas, selectedIndex, selected, returnService;
+
+    victimas = [];
+
+    selected = 0;
+
+    returnService = {};
+
+    returnService.victimas = victimas;
+    returnService.selected = selected;
+    returnService.add = function () {
+        victimas[victimas.length] = {};
+    };
+    returnService.remove = function (index) {
+        victimas.splice(index, 1);
+    };
+
+    return returnService;
+});
+
+app.controller("VictimasControl", function ($scope, VictimasService) {
+    "use strict";
+    $scope.Victimas = VictimasService.victimas;
+
+    $scope.selected = VictimasService.selected;
+
+    $scope.select = function (index) {
+        $scope.selected = index;
+    };
+
+    $scope.add = function () {
+        $scope.selected = VictimasService.victimas.length;
+        VictimasService.add();
+    };
+
+    $scope.remove = function (index) {
+        VictimasService.remove(index);
+    };
+
+});
+
+app.factory('DetenidosService', function () {
+    "use strict";
+    var detenidos, selectedIndex, selected, returnService;
+
+    detenidos = [];
+
+    selected = 0;
+
+    returnService = {};
+
+    returnService.detenidos = detenidos;
+    returnService.selected = selected;
+    returnService.add = function () {
+        detenidos[detenidos.length] = {};
+    };
+    returnService.remove = function (index) {
+        detenidos.splice(index, 1);
+    };
+
+    return returnService;
+});
+
+app.controller("DetenidosControl", function ($scope, DetenidosService) {
+    "use strict";
+    $scope.Detenidos = DetenidosService.detenidos;
+
+    $scope.selected = DetenidosService.selected;
+
+    $scope.select = function (index) {
+        $scope.selected = index;
+    };
+
+    $scope.add = function () {
+        $scope.selected = DetenidosService.detenidos.length;
+        DetenidosService.add();
+    };
+
+    $scope.remove = function (index) {
+        DetenidosService.remove(index);
+    };
+
+});
+
+app.factory('TestigosService', function () {
+    "use strict";
+    var testigos, selectedIndex, selected, returnService;
+
+    testigos = [];
+
+    selected = 0;
+
+    returnService = {};
+
+    returnService.testigos = testigos;
+    returnService.selected = selected;
+    returnService.add = function () {
+        testigos[testigos.length] = {};
+    };
+    returnService.remove = function (index) {
+        testigos.splice(index, 1);
+    };
+
+    return returnService;
+});
+
+app.controller("TestigosControl", function ($scope, TestigosService) {
+    "use strict";
+    $scope.Testigos = TestigosService.testigos;
+
+    $scope.selected = TestigosService.selected;
+
+    $scope.select = function (index) {
+        $scope.selected = index;
+    };
+
+    $scope.add = function () {
+        $scope.selected = TestigosService.testigos.length;
+        TestigosService.add();
+    };
+
+    $scope.remove = function (index) {
+        TestigosService.remove(index);
+    };
+
+});
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var app = angular.module('App', ['uiGmapgoogle-maps']);
 
-app.controller("controller", function ($scope) {
-  angular.extend($scope, {
-    map: {
-      center: {
-        latitude: 45,
-        longitude: -73
-      },
-      zoom:8,
-    }
-  });
-})
+app.controller("controller", function ($scope, $timeout) {
+    angular.extend($scope, {
+        map: {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8,
+            markers: [
+                {
+                  latitude: 41.169444444444444,
+                  longitude: -75.87777777777778,
+            
+                }
+            ]
+        }
+    });
+});
 
 
 app.controller("SensorController", function ($scope) {
@@ -19,6 +26,15 @@ app.controller("SensorController", function ($scope) {
     $scope.Humedad = 54;
     $scope.Presion = 811.3;
     $scope.Position = "20°39′58″N 103°21′07″O";
+    
+
+    navigator.geolocation.watchPosition(function (pos) {
+    
+            window.alert("0");
+            $scope.$apply(function () {
+            $scope.Position = pos.coords.latitude + "," + pos.coords.longitude;
+        });
+    });
 });
 
  
